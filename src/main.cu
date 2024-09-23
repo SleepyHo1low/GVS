acc5187bd8ef7bda4e2dc62d4a80aa57434a074d
Бажен <95105368+Tam15937@users.noreply.github.com>
2024-09-23 13:08:36 +0300

    unsigned long long int

--- src/main.cu
+++ src/main.cu
@@ -21,7 +21,7 @@ int main()
 {
     srand(time(0));
 
-    const long int N = 5000000000;
+    const unsigned long long int N = 5000000000;
     const long int floatS = N*sizeof(float);
 
     float *A = new float[N];
