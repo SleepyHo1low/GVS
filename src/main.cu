b7c86aeee381ca112c54d407109aec6d4b26f861
AmbientNomadA12 <119795304+AmbientNomadA12@users.noreply.github.com>
2024-09-23 13:10:09 +0700

    Update main.cu

--- src/main.cu
+++ src/main.cu
@@ -22,7 +22,7 @@ int main()
     srand(time(0));
 
     //Ðàçìåð âåêòîðîâ
-    const int N = INT_MAX;
+    const int N = 100000;
     const long int floatS = N*sizeof(float);
 
     float *A = new float[N];
