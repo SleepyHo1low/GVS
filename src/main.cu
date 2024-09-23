a3e174d705e5832ddc2b314cf5323db9ffeb6f06
AmbientNomadA12 <119795304+AmbientNomadA12@users.noreply.github.com>
2024-09-23 13:23:44 +0700

    Update main.cu

--- src/main.cu
+++ src/main.cu
@@ -28,7 +28,7 @@ int main()
     float *A = new float[N];
     float *B = new float[N];
 
-    float answerCPU, *answerGPU = 0, *answerGGPU = -1;
+    float answerCPU, *answerGPU = 0, *answerGGPU = 0;
 
     fillArrays(A, B, N);
 
