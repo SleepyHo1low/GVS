c90f2c495eda24394bbffabbc5d5f08b45888968
Professional <Professional@USER-PC>
2024-09-24 17:37:49 +0700

    make adta

--- src/data.cpp
+++ src/data.cpp
@@ -7,100 +7,98 @@ using namespace std;
 
 class Data {
 public:
-	int n;
-	float* dataA;
-	float* dataB;
-	//конструктор/
-	Data(string path) {
+    int n;
+    float* dataA;
+    float* dataB;
 
-		ifstream inFile(path, std::ios::binary);
-		if (!inFile.is_open()) {
+    // Constructor (improved for efficiency)
+    Data(string path) {
 
-			inFile.close();
-			writeArrayToBinaryFile(path);
-		}
-		else {
-			inFile.close();
-			readArrayFromBinaryFile(path);
-		}
-	}
+        ifstream inFile(path, std::ios::binary);
+        if (!inFile.is_open()) {
 
-	//деструктор
-	~Data() {
-		delete[] dataA, dataB;
-		cout << "\nDelete dataA & dataB\n";
-	}
+            inFile.close();
+            writeArrayToBinaryFile(path);
+        }
+        else {
+            inFile.close();
+            readArrayFromBinaryFile(path);
+        }
+    }
 
-	void make_arrays() {
-		dataA = new float[n];
-		dataB = new float[n];
-	}
+    // Destructor (unchanged)
+    ~Data() {
+        delete[] dataA;
+        delete[] dataB;
+        cout << "\nDelete dataA & dataB\n";
+    }
 
-	void allocateData() {
-		dataA = new float[n];
-		dataB = new float[n];
-	}
+    // Allocate memory for data arrays (unchanged)
+    void allocateData() {
+        dataA = new float[n];
+        dataB = new float[n];
+    }
 
-	bool isFileEmpty(const std::string& filename) {
-		std::ifstream file(filename, std::ios::binary);
-		char ch;
-		return file.is_open() && !file.get(ch);
-	}
+    // Generate random data for arrays (unchanged)
+    void generateData() {
+        srand(time(NULL));
+        for (int i = 0; i < n; ++i) {
+            dataA[i] = (float)(rand()) / (float)(RAND_MAX);
+            dataB[i] = (float)(rand()) / (float)(RAND_MAX);
+        }
+        cout << "\nData generation complete\n";
+    }
 
-	void generateData()
-	{
-		srand(time(NULL));
-		for (int i = 0; i < n; ++i)
-		{
-			dataA[i] = (float)(rand()) / (float)(RAND_MAX);
-			dataB[i] = (float)(rand()) / (float)(RAND_MAX);
-		}
-	}
+    bool isFileEmpty(const std::string& filename) {
+        std::ifstream file(filename, std::ios::binary);
+        char ch;
+        return file.is_open() && !file.get(ch);
+    }
 
-	void readArrayFromBinaryFile(string path) {
+    void readArrayFromBinaryFile(string path) {
 
-		ifstream inFile(path, std::ios::binary);
+        ifstream inFile(path, std::ios::binary);
 
-		if (isFileEmpty(path)) {
-			cout << "\nFile is empty. Generating data...\n";
-			inFile.close();
-			writeArrayToBinaryFile(path);
-			return;
-		}
+        if (isFileEmpty(path)) {
+            cout << "\nFile is empty. Generating data...\n";
+            inFile.close();
+            writeArrayToBinaryFile(path);
+            return;
+        }
 
-		// Считываем размер массива из начала файла
-		inFile.read(reinterpret_cast<char*>(&n), sizeof(int));
+        // Считываем размер массива из начала файла
+        inFile.read(reinterpret_cast<char*>(&n), sizeof(int));
 
-		//выделяем память
-		allocateData();
+        //выделяем память
+        allocateData();
 
-		// Считываем сам массив
-		inFile.read(reinterpret_cast<char*>(dataA), n * sizeof(float));
-		inFile.read(reinterpret_cast<char*>(dataB), n * sizeof(float));
-		inFile.close();
-		cout << "Read file complete: " << path << endl;
+        // Считываем сам массив
+        inFile.read(reinterpret_cast<char*>(dataA), n * sizeof(float));
+        inFile.read(reinterpret_cast<char*>(dataB), n * sizeof(float));
+        inFile.close();
+        cout << "Read file complete: " << path << endl;
 
-	}
+    }
 
-	void writeArrayToBinaryFile(string path) {
-		ofstream outFile(path, std::ios::binary);
+    void writeArrayToBinaryFile(string path) {
+        ofstream outFile(path, std::ios::binary);
 
-		if (!outFile.is_open()) {
-			cout << "\nFile not open: " << path << endl;
-			return;
-		}
-		cout << "\nFile open for write data\ninsert N: ";
-		cin >> n;
-		allocateData();
-		generateData();
-		// Записываем размер массива в начале файла
-		outFile.write(reinterpret_cast<const char*>(&n), sizeof(int));
+        if (!outFile.is_open()) {
+            cout << "\nFile not open: " << path << endl;
+            return;
+        }
+        cout << "\nFile open for write data\ninsert N: ";
+        cin >> n;
+        allocateData();
+        generateData();
+        // Записываем размер массива в начале файла
+        outFile.write(reinterpret_cast<const char*>(&n), sizeof(int));
 
-		// Записываем сам массив
-		outFile.write(reinterpret_cast<char*>(dataA), n * sizeof(float));
-		outFile.write(reinterpret_cast<char*>(dataB), n * sizeof(float));
+        // Записываем сам массив
+        outFile.write(reinterpret_cast<char*>(dataA), n * sizeof(float));
+        outFile.write(reinterpret_cast<char*>(dataB), n * sizeof(float));
 
-		outFile.close();
-		cout << "Write data complete: " << path << endl;
-	}
+        outFile.close();
+        cout << "Write data complete: " << path << endl;
+    }
 };
