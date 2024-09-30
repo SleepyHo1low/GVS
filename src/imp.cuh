3aeb6f2c96343432775a1780889f9a2ac8324a08
Бажен <95105368+Tam15937@users.noreply.github.com>
2024-09-30 14:54:48 +0700

    128

--- src/imp.cuh
+++ src/imp.cuh
@@ -4,7 +4,8 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-#define THREADS_PER_BLOCK 256
+#define THREADS_PER_BLOCK 128
+//#define BLOCKS_PER_GRID ((1000000000 + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK)
 __shared__ float partialSums[THREADS_PER_BLOCK];
 float CPUimplementation(float* a, float* b, int N);
 __global__ void GPUimplementation(float* a, float* b, float* result);
