997ffd636e76c715dc4830f3b5fd7cfc7cdd0f47
Бажен <95105368+Tam15937@users.noreply.github.com>
2024-09-30 10:02:22 +0300

    Update imp.cu

--- src/imp.cu
+++ src/imp.cu
@@ -1,16 +1,15 @@
 #include "imp.cuh"
-__shared__ float partialSums[THREADS_PER_BLOCK];
 
 __global__ void GPUimplementation(float* a, float* b, float* result)
 {
         int tid = threadIdx.x;
     int i = blockIdx.x * blockDim.x + tid;
 
-    // Âû÷èñëåíèå ëîêàëüíîé ñóììû
+    // Ã‚Ã»Ã·Ã¨Ã±Ã«Ã¥Ã­Ã¨Ã¥ Ã«Ã®ÃªÃ Ã«Ã¼Ã­Ã®Ã© Ã±Ã³Ã¬Ã¬Ã»
     partialSums[tid] = a[i] * b[i];
     __syncthreads();
 
-    // Ñóììèðîâàíèå ðåçóëüòàòîâ â ïðåäåëàõ áëîêà
+    // Ã‘Ã³Ã¬Ã¬Ã¨Ã°Ã®Ã¢Ã Ã­Ã¨Ã¥ Ã°Ã¥Ã§Ã³Ã«Ã¼Ã²Ã Ã²Ã®Ã¢ Ã¢ Ã¯Ã°Ã¥Ã¤Ã¥Ã«Ã Ãµ Ã¡Ã«Ã®ÃªÃ 
     for (int s = blockDim.x / 2; s > 0; s >>= 1) {
         if (tid < s) {
             partialSums[tid] += partialSums[tid + s];
@@ -18,7 +17,7 @@ __global__ void GPUimplementation(float* a, float* b, float* result)
         __syncthreads();
     }
 
-    // Çàïèñü ðåçóëüòàòà â ãëîáàëüíóþ ïàìÿòü
+    // Ã‡Ã Ã¯Ã¨Ã±Ã¼ Ã°Ã¥Ã§Ã³Ã«Ã¼Ã²Ã Ã²Ã  Ã¢ Ã£Ã«Ã®Ã¡Ã Ã«Ã¼Ã­Ã³Ã¾ Ã¯Ã Ã¬Ã¿Ã²Ã¼
     if (tid == 0) {
         atomicAdd(result, partialSums[0]);
     }
