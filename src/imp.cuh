c32042f770410c0407bebc084dbfe7557430a7b2
Бажен <95105368+Tam15937@users.noreply.github.com>
2024-09-30 10:02:09 +0300

    Update imp.cuh

--- src/imp.cuh
+++ src/imp.cuh
@@ -4,6 +4,7 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
+__shared__ float partialSums[THREADS_PER_BLOCK];
 float CPUimplementation(float* a, float* b, int N);
 __global__ void GPUimplementation(float* a, float* b, float* result);
 
