0f0e4366ea5ba06309a0ff91f34ee73713ab0cb4
Бажен <95105368+Tam15937@users.noreply.github.com>
2024-09-30 17:36:46 +0700

    sdfghg

--- src/imp.cuh
+++ src/imp.cuh
@@ -4,9 +4,9 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-#define THREADS_PER_BLOCK 256;
+#define THREADS_PER_BLOCK 256
 float CPUimplementation(float* a, float* b, int N);
 __global__ void GPUimplementation(float *a, float *b, float *result, int n);
-__global__ void GPUimplementation(float* a, float* b, float* result, int N);
+__global__ void GPUatomicimplementation(float* a, float* b, float* result, int N);
 
 #endif 
