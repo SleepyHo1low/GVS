1b599ad26abce3a605d0713aaef1510b2ae1b1ee
Бажен <95105368+Tam15937@users.noreply.github.com>
2024-09-30 16:42:33 +0700

    вапрол

--- src/imp.cu
+++ src/imp.cu
@@ -2,25 +2,27 @@
 
 __global__ void GPUimplementation(float* a, float* b, float* result, int N)
 {
+    __shared__ float partialSums[THREADS_PER_BLOCK];
 
-    // Инициализация разделяемой памяти нулями
-    partialSums[threadIdx.x] = 0.0f;
     int tid = threadIdx.x;
     int i = blockIdx.x * blockDim.x + tid;
 
-    if( i<N){
+    // Инициализация разделяемой памяти нулями
+    partialSums[tid] = 0.0f;
 
+    if (i < N) {
         // Вычисление локальной суммы
         partialSums[tid] = a[i] * b[i];
 
         __syncthreads();
+
         // Суммирование результатов в пределах блока
-        if( tid ==0){
-        
-            int sum =0;
-            for( int i=0; i<TREADS_PER_BLOCK; i++){
-                sum+=partialSums[i];
+        if (tid == 0) {
+            int sum = 0;
+            for (int i = 0; i < THREADS_PER_BLOCK; i++) {
+                sum += partialSums[i];
             }
+            __syncthreads(); // Синхронизация перед атомарной операцией
             atomicAdd(result, sum);
         }
     }
--- src/imp.cuh
+++ src/imp.cuh
@@ -5,7 +5,6 @@
 #include "device_launch_parameters.h"
 
 #define TREADS_PER_BLOCK 256
-__shared__ float partialSums[TREADS_PER_BLOCK];
 float CPUimplementation(float* a, float* b, int N);
 __global__ void GPUimplementation(float* a, float* b, float* result, int N);
 
