84f8bd238b1421f13c1e3dbb5b2c0f98a93c7673
Бажен <95105368+Tam15937@users.noreply.github.com>
2024-09-30 14:00:47 +0700

    переделал GPUimplementation

--- src/imp.cu
+++ src/imp.cu
@@ -1,9 +1,27 @@
 #include "imp.cuh"
+__shared__ float partialSums[THREADS_PER_BLOCK];
 
 __global__ void GPUimplementation(float* a, float* b, float* result)
 {
-    int i = threadIdx.x + blockDim.x * blockIdx.x;
-    atomicAdd(result, a[i] * b[i]);
+        int tid = threadIdx.x;
+    int i = blockIdx.x * blockDim.x + tid;
+
+    // Вычисление локальной суммы
+    partialSums[tid] = a[i] * b[i];
+    __syncthreads();
+
+    // Суммирование результатов в пределах блока
+    for (int s = blockDim.x / 2; s > 0; s >>= 1) {
+        if (tid < s) {
+            partialSums[tid] += partialSums[tid + s];
+        }
+        __syncthreads();
+    }
+
+    // Запись результата в глобальную память
+    if (tid == 0) {
+        atomicAdd(result, partialSums[0]);
+    }
 }
 
 float CPUimplementation(float* a, float* b, int N)
