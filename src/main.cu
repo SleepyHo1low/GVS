26a6cc07d05766fc08d1d2831c4cb7cecd5319c6
Бажен <95105368+Tam15937@users.noreply.github.com>
2024-09-30 16:32:50 +0700

    sdfghjm

--- src/main.cu
+++ src/main.cu
@@ -47,6 +47,8 @@ int main()
 
     int number_of_blocks = N / TREADS_PER_BLOCK + 1;
 
+    cout<<"TREADS_PER_BLOCK = "<<TREADS_PER_BLOCK<<endl;
+    cout<<"number_of_blocks = "<<number_of_blocks<<endl;
     cudaEvent_t startGPU, stopGPU;
     cudaEventCreate(&startGPU);
     cudaEventCreate(&stopGPU);
