696d1228350cb213460bd1d5c94ef60d1b98cc81
Бажен <95105368+Tam15937@users.noreply.github.com>
2024-09-30 17:00:50 +0700

    5

--- src/main.cu
+++ src/main.cu
@@ -33,7 +33,7 @@ int main()
     //GPU
     const int floatS = N*sizeof(float);
     float *answerGPU, *cudaA, *cudaB;
-    float answerGGPU =0;
+    float answerGGPU =5;
 
     cudaMalloc(&cudaA, floatS);
     cudaMalloc(&cudaB, floatS);
