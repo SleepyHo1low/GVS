917bddce4b7686f021b19b995f2e3327f960aa77
Бажен <95105368+Tam15937@users.noreply.github.com>
2024-09-30 15:27:48 +0700

    выарол

--- src/imp.cuh
+++ src/imp.cuh
@@ -4,7 +4,7 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-//#define THREADS_PER_BLOCK 256
+#define THREADS_PER_BLOCK 256
 //#define BLOCKS_PER_GRID ((1000000000 + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK)
 __shared__ float partialSums[THREADS_PER_BLOCK];
 float CPUimplementation(float* a, float* b, int N);
