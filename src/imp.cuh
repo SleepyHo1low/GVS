25b392eb5da7296a1fa862189f76b63abafa09c7
Бажен <95105368+Tam15937@users.noreply.github.com>
2024-09-30 15:43:31 +0700

    dfhj

--- src/imp.cuh
+++ src/imp.cuh
@@ -5,7 +5,7 @@
 #include "device_launch_parameters.h"
 
 #define THREADS_PER_BLOCK 256
-//#define BLOCKS_PER_GRID ((1000000000 + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK)
+#define BLOCKS_PER_GRID ((1000000000 + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK)
 __shared__ float partialSums[THREADS_PER_BLOCK];
 float CPUimplementation(float* a, float* b, int N);
 __global__ void GPUimplementation(float* a, float* b, float* result, int N);
