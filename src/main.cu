34ce13cea0b0dc00fc9006be5b699746f4edf98a
Professional <Professional@USER-PC>
2024-10-02 22:50:43 +0700

    make func CPUcalc and GPUcalc, and realizeGPUatomic

--- src/main.cu
+++ src/main.cu
@@ -5,9 +5,72 @@
 #include <curand.h>
 #include "imp.cuh"
 #include "data.cpp"
+
 using namespace std;
 using namespace std::chrono;
 
+void CPUcalc(float* A, float* B, int N,float &answerCPU) {
+    //CPU
+    auto start = high_resolution_clock::now();
+    answerCPU = CPUimplementation(A, B, N);
+    auto stop = high_resolution_clock::now();
+    int ms = duration_cast<milliseconds>(stop - start).count();
+    int ns = duration_cast<nanoseconds>(stop - start).count();
+    cout << "Num elements: " << N << endl;
+    cout << "Answer (CPU): " << answerCPU << " time: " <<
+        ms << "." << ns - ms * 1000000 << " ms" << endl;
+    return;
+}
+
+void GPUcalc(float* A, float* B, int N, float& answerGGPU,bool is_atomic) {
+    //GPU
+    const int floatS = N * sizeof(float);
+    float* answerGPU, * cudaA, * cudaB;
+    
+
+    cudaMalloc(&cudaA, floatS);
+    cudaMalloc(&cudaB, floatS);
+    cudaMalloc(&answerGPU, sizeof(float));
+
+    cudaMemcpy(cudaA, A, floatS, cudaMemcpyHostToDevice);
+    cudaMemcpy(cudaB, B, floatS, cudaMemcpyHostToDevice);
+
+    cudaMemset(answerGPU, 0, sizeof(float));
+
+    int number_of_blocks = (N + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK;;
+
+    cudaEvent_t startGPU, stopGPU;
+    cudaEventCreate(&startGPU);
+    cudaEventCreate(&stopGPU);
+
+    cudaEventRecord(startGPU, 0);
+
+    is_atomic ?
+        GPUatomicimplementation << < number_of_blocks, THREADS_PER_BLOCK >> > (cudaA, cudaB, answerGPU, N) :
+        GPUimplementation << < number_of_blocks, THREADS_PER_BLOCK, THREADS_PER_BLOCK * sizeof(float) >> > (cudaA, cudaB, answerGPU, N);
+    cudaEventRecord(stopGPU, 0);
+    cudaEventSynchronize(stopGPU);
+
+    // Проверка ошибок
+    cudaError_t error = cudaGetLastError();
+    if (error != cudaSuccess) {
+        std::cout << "CUDA error: " << cudaGetErrorString(error) << std::endl;
+        system("pause");
+        exit(-1);
+    }
+
+    cudaMemcpy(&answerGGPU, answerGPU, sizeof(float), cudaMemcpyDeviceToHost);
+
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, startGPU, stopGPU);
+    is_atomic ?
+        cout << "Answer (GPUatomic): " << answerGGPU << " time: " << milliseconds << " ms" << endl :
+        cout << "Answer (GPU): " << answerGGPU << " time: " << milliseconds << " ms" << endl;
+    cudaFree(cudaA);
+    cudaFree(cudaB);
+    cudaFree(answerGPU);
+    return;
+}
 
 int main()
 {
@@ -31,64 +94,12 @@ int main()
             float* B = data.dataB;
 
             float answerCPU;
-
-
-
-            //CPU
-            auto start = high_resolution_clock::now();
-            answerCPU = CPUimplementation(A, B, N);
-            auto stop = high_resolution_clock::now();
-            int ms = duration_cast<milliseconds>(stop - start).count();
-            int ns = duration_cast<nanoseconds>(stop - start).count();
-            cout << "Num elements: " << N << endl;
-            cout << "Answer (CPU): " << answerCPU << " time: " <<
-                ms << "." << ns - ms * 1000000 << " ms" << endl;
-            //cout<<" TEST " << ms <<" " << ns << " TEST" << endl;
-            //GPU
-            const int floatS = N * sizeof(float);
-            float* answerGPU, * cudaA, * cudaB;
+            CPUcalc(A, B, N, answerCPU);
             float answerGGPU;
-
-            cudaMalloc(&cudaA, floatS);
-            cudaMalloc(&cudaB, floatS);
-            cudaMalloc(&answerGPU, sizeof(float));
-
-            cudaMemcpy(cudaA, A, floatS, cudaMemcpyHostToDevice);
-            cudaMemcpy(cudaB, B, floatS, cudaMemcpyHostToDevice);
-
-            cudaMemset(answerGPU, 0, sizeof(float));
-
-            int number_of_blocks = (N + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK;;
-
-            //cout<<"THREADS_PER_BLOCK = "<<THREADS_PER_BLOCK<<endl;
-            //cout<<"number_of_blocks = "<<number_of_blocks<<endl;
-            cudaEvent_t startGPU, stopGPU;
-            cudaEventCreate(&startGPU);
-            cudaEventCreate(&stopGPU);
-
-            cudaEventRecord(startGPU,0);
-            //GPUatomicimplementation << < number_of_blocks, THREADS_PER_BLOCK, THREADS_PER_BLOCK * sizeof(float) >> > (cudaA, cudaB, answerGPU, N);
-            GPUimplementation << < number_of_blocks, THREADS_PER_BLOCK, THREADS_PER_BLOCK * sizeof(float) >> > (cudaA, cudaB, answerGPU, N);
-            cudaEventRecord(stopGPU,0);
-            cudaEventSynchronize(stopGPU);
-
-            // Проверка ошибок
-            cudaError_t error = cudaGetLastError();
-            if (error != cudaSuccess) {
-                std::cout << "CUDA error: " << cudaGetErrorString(error) << std::endl;
-                return 1;
-            }
-
-            cudaMemcpy(&answerGGPU, answerGPU, sizeof(float), cudaMemcpyDeviceToHost);
-
-            float milliseconds = 0;
-            cudaEventElapsedTime(&milliseconds, startGPU, stopGPU);
-            
-            cout << "Answer (GPU): " << answerGGPU << " time: " << milliseconds << " ms" << endl;
+            GPUcalc(A, B, N, answerGGPU,true);
             cout << "Diff (CPU - GPU): " << answerCPU - answerGGPU << endl;
-            cudaFree(cudaA);
-            cudaFree(cudaB);
-            cudaFree(answerGPU);
+            GPUcalc(A, B, N, answerGGPU, false);
+            cout << "Diff (CPU - GPU): " << answerCPU - answerGGPU << endl;
         }
         else
             cout << "Incorrect input:" << action;
